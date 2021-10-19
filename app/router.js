c200a8731a2b3b8a52ca789d5ccfb67d1df445ca
Dananando <dan.andriamihaja@gmail.com>
2021-10-20 00:14:50 +0200

    Skeleton of the API setup

---
+++ app/router.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = Router();
+
+// Test page
+router.get('/', (_, response) => {
+    response.send('It\'s running');
+});
+
+// Route 404
+router.use((request, response) => {
+    response.status(404).json(`Endpoint ${request.url} not found`);
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,24 @@
+// Require and setup of express
+const express = require('express');
+const app = express();
+
+// dotenv to use the .env file
+require('dotenv').config();
+
+// Where the routes are defined
+const router = require('./app/router');
+
+const PORT = process.env.PORT || 4444;
+
+// MW to use the json data the app will send
+app.use(express.json());
+
+// MW to read correctly request.body
+app.use(express.urlencoded({ extended: true }));
+
+// It is the version 1 of our API
+app.use('/v1', router);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}.`)
+});
