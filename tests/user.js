d29b57ffb3367ef03203e5c555002174dd81999d
Dananando <dan.andriamihaja@gmail.com>
2021-10-22 18:14:31 +0200

    A few corrections

--- tests/user.js
+++ tests/user.js
@@ -13,8 +13,8 @@ describe('User signup, authentication, deletion methods', () => {
   describe('POST /api/signup', () => {
     it('It should create a new user and return an object containing its mail', (done) => {
       const newUser = {
-        email: 'test5@test.com',
-        password: 'testpassword',
+        email: 'lili@andria.com',
+        password: 'password',
       };
       chai.request(server)
         .post('/v1/api/signup')
@@ -24,8 +24,8 @@ describe('User signup, authentication, deletion methods', () => {
           response.body.should.be.a('object');
           response.body.should.have.property('id');
           response.body.should.have.property('email');
-          response.body.should.have.property('id').eq(11);
-          response.body.should.have.property('email').eq('test5@test.com');
+          response.body.should.have.property('id').eq(2);
+          response.body.should.have.property('email').eq('lili@andria.com');
           done();
         });
     });
@@ -34,8 +34,41 @@ describe('User signup, authentication, deletion methods', () => {
   /**
    * POST - Test authentication
    */
+   describe('POST /api/token', () => {
+    it('It should authenticate a user and return a token', (done) => {
+      const userToAuthenticate = {
+        email: 'lili@andria.com',
+        password: 'password',
+      };
+      chai.request(server)
+        .post('/v1/api/token')
+        .send(userToAuthenticate)
+        .end((_, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a('object');
+          response.body.should.have.property('logged');
+          response.body.should.have.property('id');
+          response.body.should.have.property('email').eq('lili@andria.com');
+          done();
+        });
+    });
+  });
 
   /**
    * DELETE - Delete a user
    */
+   describe('DELETE /api/user/:id', () => {
+    it('It should delete a user and return its former id', (done) => {
+      const id = 2;
+      chai.request(server)
+        .delete(`/v1/api/user/${id}`)
+        .end((_, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a('object');
+          response.body.should.have.property('id');
+          response.body.should.have.property('id').eq(2);
+          done();
+        });
+    });
+  });
 });
